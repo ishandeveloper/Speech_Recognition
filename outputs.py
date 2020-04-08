5a336ef80c22fa17020b3c7643022b556d9eb836
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 20:24:12 +0530

    Output Methods

--- outputs.py
+++ outputs.py
@@ -8,8 +8,10 @@ def say(response):
     tts=gTTS(str(response),lang='en')
     r=random.randint(1,1000000)
     audio_file='audio-'+str(r) + '.mp3';
-
+    tts.save(audio_file)
     print('Jarvis >> '+response)
+    playsound.playsound(audio_file)
+    os.remove(audio_file)
 
 def heard(response):
     print("You    >> "+response)
