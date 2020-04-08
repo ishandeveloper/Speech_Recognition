0098df8a5815989a43e30299fe290d53ac3425a8
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 19:21:42 +0530

    Created Listen Audio Function

---
+++ main.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+
+r=sr.Recognizer()
+
+def listen_audio():
+    with sr.Microphone() as source:
+        audio=r.listen(source)
+        voice_data=''
+        try:
+            voice_data=r.recognize_google(audio)
+            print(voice_data)
+        except sr.UnknownValueError:
+            print("I didn't get that")
+        except sr.RequestError:
+            print('Sorry, Speech Service Is Down')
+        return voice_data
+
+print('How Can I Help You ?')
+voice_data=record_audio()
+
+        
+
+        
