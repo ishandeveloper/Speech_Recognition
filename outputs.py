7a31e6264cd49124e69ec010fb1873bf0e38eb83
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 20:11:39 +0530

    Added Text To Speech

--- outputs.py
+++ outputs.py
@@ -1,4 +1,14 @@
+import playsound
+import os
+import random
+from gtts import gTTS
+
+
 def say(response):
+    tts=gTTS(str(response),lang='en')
+    r=random.randint(1,1000000)
+    audio_file='audio-'+str(r) + '.mp3';
+
     print('Jarvis >> '+response)
 
 def heard(response):
