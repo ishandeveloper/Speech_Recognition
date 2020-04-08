1571f50a686ad46034f0d5f3690c2b5e05fb92a2
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 19:52:33 +0530

    Listener Events

---
+++ listener.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+from outputs import *
+r=sr.Recognizer()
+
+def listen_audio():
+    with sr.Microphone() as source:
+        audio=r.listen(source)
+        voice_data=''
+        try:
+            voice_data=r.recognize_google(audio)
+        except sr.UnknownValueError:
+            say("I didn't get that")
+        except sr.RequestError:
+            say('Sorry, Speech Service Is Down')
+        
+        heard(voice_data)
+        return voice_data
