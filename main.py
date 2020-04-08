2051310434cf206769742d0d2975954255e8bd7d
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 20:11:24 +0530

    main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from listener import listen_audio
 from outputs import say
 import time
 
-time.sleep(1)
-say("How Can I Help You ?")
-while 1:
-    voice_data=listen_audio()
-    respond(voice_data)
-
+def listeningloop():
+    time.sleep(1)
+    say("How Can I Help You ?")
+    while 1:
+        voice_data=listen_audio()
+        respond(voice_data)
         
+
+listeningloop()
 
         
