9b106c7b674dbb601771b0945a4dee1b767f22f3
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 19:52:58 +0530

    Response Events

---
+++ responses.py
@@ -0,0 +1,23 @@
+from time import ctime
+from listener import listen_audio
+import webbrowser
+from outputs import say
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        say("Hey, I'm Jarvis.")
+    if 'time' in voice_data:
+        say(ctime())
+    if 'search' in voice_data:
+        say('What do you want to search for?')
+        search=listen_audio()
+        url='https://google.com/search?q='+search
+        webbrowser.get().open(url)
+        say("Here's what I found for "+search)
+    if ('find location' or 'search for location' or 'the location' or 'find place') in voice_data:
+        say('What Is The Location?')
+        location=listen_audio()
+        url='https://google.com/maps/place/'+location+'/&amp;'
+        webbrowser.get().open(url)
+        say("Here's The Location Of "+location)
+    
